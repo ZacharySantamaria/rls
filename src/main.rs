93be5a68976886df72b646d5f8049f129939cdf4
Zachary Santamaria <zsantamaria@protonmail.com>
2022-05-28 03:32:22 -0400

    Continuing to make this better. Added the feature of having metadata to the design. Added a amount of bytes to be displayed.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use std::fs;
 
 fn main() {
-    let paths = fs::read_dir(".").unwrap();
-
-    for path in paths {
-
-        println!("Name is {}", path.unwrap().path().display())
-
+    if let Ok(entries) = fs::read_dir(".") {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                // Here, `entry` is a `DirEntry`.
+                if let Ok(metadata) = entry.metadata() {
+                    // Now let's show our entry's permissions!
+                    println!("{:?} {:?} bytes", entry.path(), metadata.len());
+                } else {
+                    println!("Couldn't get metadata for {:?}", entry.path());
+                }
+            }
+        }
     }
+    
+
 }
